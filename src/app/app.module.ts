448e8835a7a92a03e6f2f3fdeffe86aa3072dda1
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-18 13:33:52 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitSerachComponent } from './git-serach/git-serach.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitSerachComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/git-serach/git-serach.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-git-serach',
+  templateUrl: './git-serach.component.html',
+  styleUrls: ['./git-serach.component.css']
+})
+export class GitSerachComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

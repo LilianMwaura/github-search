ec297e3c5b933dd1593eb4570d07d93ab69a7650
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-19 06:54:42 +0300

    create routes for repository

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepositoryComponent } from './repository/repository.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
     { path:'users', component: UsersComponent},
+    { path: 'repository', component: RepositoryComponent},
+    { path: '', redirectTo:"/github", pathMatch:"full"},
     
 ];
 
